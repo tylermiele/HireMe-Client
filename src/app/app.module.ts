9e1c14becab207002aef3da6ca619eef180d26a5
Tyler Miele <tylermiele10@gmail.com>
2023-03-24 15:55:27 -0400

    Added client files

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -2,15 +2,17 @@ import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
 
 import { AppComponent } from './app.component';
+import { EmployerComponent } from './employer/employer.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    EmployerComponent
   ],
   imports: [
     BrowserModule
   ],
   providers: [],
-  bootstrap: [AppComponent]
+  bootstrap: [EmployerComponent]
 })
 export class AppModule { }

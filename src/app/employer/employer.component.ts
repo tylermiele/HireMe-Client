8157c1697690462f2591a5b6f70498da718174c6
Tyler Miele <tylermiele10@gmail.com>
2023-03-31 16:33:23 -0400

    Finished CRUD operations

--- src/app/employer/employer.component.ts
+++ src/app/employer/employer.component.ts
@@ -11,6 +11,7 @@ import { EmployerService } from '../services/employer.service';
 export class EmployerComponent implements OnInit {
   constructor(private service: EmployerService) { }
   employers: any;
+  _id: string | undefined;
   name: string | undefined;
   city: string | undefined;
   contactEmail: string | undefined;
@@ -24,7 +25,7 @@ export class EmployerComponent implements OnInit {
     });
   };
 
-  addEmployer(): void{
+  addEmployer(): void {
     // populate the new employer
     let employer = {
       name: this.name,
@@ -41,8 +42,43 @@ export class EmployerComponent implements OnInit {
     });
 
   }
+  selectEmployer(_id: string, name: string, city: string, contactEmail: string, comments: string, employees: number): void {
+    this._id = _id;
+    this.name = name;
+    this.city = city;
+    this.contactEmail = contactEmail;
+    this.comments = comments;
+    this.employees = employees;
+  }
 
-  reset(): void{
+  deleteEmployer(_id: string) {
+    if (confirm('Are you sure you want to delete?')) {
+      this.service.deleteEmployer(_id).subscribe(response => {
+        this.getEmployers();
+        this.reset();
+      });
+    }
+  }
+
+  updateEmployer(_id: string): void {
+    // populate the new employer
+    let employer = {
+      name: this.name,
+      city: this.city,
+      contactEmail: this.contactEmail,
+      comments: this.comments,
+      employees: this.employees
+    };
+
+    // send it to the service which posts it to the api using PUT
+    this.service.updateEmployer(_id, employer).subscribe(response => {
+      this.getEmployers();
+      this.reset();
+    });
+
+  }
+  reset(): void {
+    this._id = undefined;
     this.name = undefined;
     this.city = undefined;
     this.contactEmail = undefined;
--- src/app/services/employer.service.ts
+++ src/app/services/employer.service.ts
@@ -10,10 +10,20 @@ import { environment } from 'src/environments/environment';
 export class EmployerService {
   serverUrl: string = environment.serverUrl;
   constructor(private http: HttpClient) { }
+
   getEmployers() {
     return this.http.get(`${this.serverUrl}/api/employers`);
   }
-  addEmployer(employer: any){
+
+  addEmployer(employer: any) {
     return this.http.post(`${this.serverUrl}/api/employers`, employer);
+  }
+
+  deleteEmployer(_id: string) {
+    return this.http.delete(`${this.serverUrl}/api/employers/${_id}`);
+  }
+  updateEmployer(_id: string, employer: any){
+    return this.http.put(`${this.serverUrl}/api/employers/${_id}`, employer);
+
   }
 }
